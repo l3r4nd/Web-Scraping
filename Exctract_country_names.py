6febbe490c1250aa51655589882572a29bbd601a
Prasad Joshi <32058627+l3r4nd@users.noreply.github.com>
2018-04-04 22:43:26 +0530

    Create Exctract_country_names.py

---
+++ Exctract_country_names.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup as bs4
+import re
+
+def Contries(link):
+    '''
+    Takes in url as input takes in searches all the Country names
+    and removes any special character.
+    '''
+    r = requests.get(link)
+    soup = bs4(r.text, 'html.parser')
+    results = soup.find_all('td')
+    pat = re.compile('(?:[A-Za-z? ])')
+    result = []
+    for index in range(0,len(results)-1,3):
+        res = results[index].text
+        result.append(''.join(re.findall(pat, res)))
+    #result.pop(-1)
+    return result[:-1]
+
+asia_ = Contries('https://www.whatarethe7continents.com/asia-continent/how-many-countries-in-asia/')
+europe_ = Contries('https://www.whatarethe7continents.com/europe/many-countries-europe/')
+africa_ = Contries('https://www.whatarethe7continents.com/africa-continent/how-many-countries-in-africa/')
+australia_ = Contries('https://www.whatarethe7continents.com/australia/many-countries-australia/')
