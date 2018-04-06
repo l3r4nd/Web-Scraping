2fc63fa9dca605ac4f4517aa1ddfc516515d8de7
Prasad Joshi <32058627+l3r4nd@users.noreply.github.com>
2018-04-06 17:52:18 +0530

    Update Exctract_country_names.py

--- Exctract_country_names.py
+++ Exctract_country_names.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs4
 import re
 
-def Contries(link):
+def Countries(link):
     '''
     Takes in url as input takes in searches all the Country names
     and removes any special character.
@@ -10,7 +10,7 @@ def Contries(link):
     r = requests.get(link)
     soup = bs4(r.text, 'html.parser')
     results = soup.find_all('td')
-    pat = re.compile('(?:[A-Za-z? ])')
+    pat = re.compile('[A-Za-z? ]')
     result = []
     for index in range(0,len(results)-1,3):
         res = results[index].text
@@ -18,7 +18,7 @@ def Contries(link):
     #result.pop(-1)
     return result[:-1]
 
-asia_ = Contries('https://www.whatarethe7continents.com/asia-continent/how-many-countries-in-asia/')
-europe_ = Contries('https://www.whatarethe7continents.com/europe/many-countries-europe/')
-africa_ = Contries('https://www.whatarethe7continents.com/africa-continent/how-many-countries-in-africa/')
-australia_ = Contries('https://www.whatarethe7continents.com/australia/many-countries-australia/')
+asia_ = Countries('https://www.whatarethe7continents.com/asia-continent/how-many-countries-in-asia/')
+europe_ = Countries('https://www.whatarethe7continents.com/europe/many-countries-europe/')
+africa_ = Countries('https://www.whatarethe7continents.com/africa-continent/how-many-countries-in-africa/')
+australia_ = Countries('https://www.whatarethe7continents.com/australia/many-countries-australia/')
